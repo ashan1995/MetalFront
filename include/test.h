c2dc0ffcbb90232887ffed8d015839f9b5804791
ashan1995 <ashan.samarasinghe95@gmail.com>
2025-04-15 00:25:41 +1000

    folder structure changed

---
+++ include/test.h
@@ -0,0 +1,10 @@
+#include <iostream>
+
+class Test {
+
+public:
+	void testfunc() {
+		std::cout << "Header file works!" << std::endl;
+	}
+
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,26 @@
+#include <iostream>
+#include <test.h>
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+int main(){
 
+    Test test;
+    test.testfunc();
+
+    std::cout << "Hello, World!" << std::endl;
+
+    sf::Window window(sf::VideoMode({ 800, 600 }), "My window");
+
+    // run the program as long as the window is open
     while (window.isOpen())
     {
+        // check all the window's events that were triggered since the last iteration of the loop
         while (const std::optional event = window.pollEvent())
         {
+            // "close requested" event: we close the window
             if (event->is<sf::Event::Closed>())
-            {
                 window.close();
-            }
         }
-
-        window.clear();
-        window.display();
     }
+    return 0;
 }
